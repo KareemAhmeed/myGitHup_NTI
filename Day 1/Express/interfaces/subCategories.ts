cc9ea52e1dbb853ce4fe4ef818de56b10ef1fe61
kareemahmed22 <kareemahmedelnagar1@gmail.com>
2024-08-31 00:30:28 +0200

    task 4 upload

--- Day 1/Express/interfaces/subCategories.ts
+++
@@ -1,6 +0,0 @@
-import { Document, Schema } from "mongoose";
-
-export interface SubCategories extends Document {
-  name: string;
-  category: Schema.Types.ObjectId;
-}
--- Day 1/Express/utils/apiErrors.ts
+++
@@ -1,9 +0,0 @@
-class ApiErrors extends Error {
-    private status: string;
-    constructor(message: string, private statusCode: number) {
-      super(message);
-      this.status = `${this.statusCode}`.startsWith('4') ? 'failed' : 'error'
-    };
-  }
-  
-  export default ApiErrors;
